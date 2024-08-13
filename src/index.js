49f11ff68d9ea8606f612b8d35cb2eccccf0b25f
mamoyojaxx <60009180+mamoyojaxx@users.noreply.github.com>
2024-08-13 13:51:42 +0200

    search using the API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
+function showWeatherData(response){
+    let temp = document.querySelector("#app_temp");
+    let tempinfo = Math.round(response.data.temperature.current);
+    let city = document.querySelector("#city")
+    city.innerHTML = response.data.city;
+    temp.innerHTML = tempinfo;
+}
+
+function searchCity(city){
+let apiKey = "d001310fae770o7ftaaab3f4b5974a90";
+let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`
+axios.get(apiURL).then(showWeatherData);
+}
+
 function onSearch(event){
 event.preventDefault();
 let searchInput = document.querySelector("#search-form-input");
-let city = document.querySelector("#city")
-city.innerHTML = searchInput.value;
+searchCity(searchInput.value);
 }
-
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", onSearch);
+
+searchCity("Harare");
